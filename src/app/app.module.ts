b45a24c4bb5df0b7f15cd1ab25f01a446c492dae
Victor Vidigal Ribeiro <victor.ribeiro1@granbery.edu.dti>
2023-08-15 22:33:16 -0300

    Incluindo angular material e bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
